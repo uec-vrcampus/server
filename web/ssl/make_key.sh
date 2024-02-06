2ecdad58f4f07d68d731c9d5390d42fb253ae190
uec-vrcampus <vrcampus@cdel.uec.ac.jp>
2024-02-06 12:08:39 +0900

    add ssl key

--- web/ssl/make_key.sh
+++ web/ssl/make_key.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 openssl req -batch -new -x509 -newkey rsa:4096 -nodes -sha256 \
-  -subj "/C=JP/ST=Tokyo/L=Chofu/O=UEC/CN=localhost" -days 3650 \
+  -subj "/C=JP/ST=Tokyo/L=Chofu/O=UEC/OU=vrcampus/CN='https:\/\/localhost:8443'" \
+  -days 3650 \
   -keyout ./server.key \
   -out ./server.crt
